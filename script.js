91d258cbbc121a6159ec3dca3006f6782ec635dd
Justine Agcanas <agcanasjustine1@gmail.com>
2024-09-13 20:34:02 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const etits = document.querySelector('.container');
+
+     etits.addEventListener('animationend', () => {
+        window.location.href = 'https://bit.ly/4d35ouL';
+    });
+});
