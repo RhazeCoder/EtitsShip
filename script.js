a058be9f861d68b578e6d2993128e84c7717c188
Justine Agcanas <agcanasjustine1@gmail.com>
2024-10-04 21:13:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const etits = document.querySelector('.container');
 
      etits.addEventListener('animationend', () => {
+        etits.style.display = "none";
         window.location.href = 'https://bit.ly/4d35ouL';
     });
 });
